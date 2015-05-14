a9a36cf1127373fedde771bd837e6e0c8e62b22b
Innos <victor.d.kazakov@gmail.com>
2015-05-14 19:15:45 +0300

    directory fix and added algorithm lab last problem from last night

--- CSharp Advanced/CSharp Advanced Debuging/CSharp Advanced Debuging/05Substring/Substring.cs
+++ CSharp Advanced/CSharp Advanced Debuging/CSharp Advanced Debuging/05Substring/Substring.cs
@@ -7,9 +7,9 @@
         public static void Main()
         {
             string text = Console.ReadLine();
-            int jump = int.Parse(Console.ReadLine());
+            int jump = int.Parse(Console.ReadLine()) + 1;
 
-            const char Search = 'р';
+            char Search = 'p';
             bool hasMatch = false;
 
             for (int i = 0; i < text.Length; i++)
@@ -20,14 +20,15 @@
 
                     int endIndex = jump;
 
-                    if (endIndex > text.Length)
+                    if (endIndex + i > text.Length)
                     {
-                        endIndex = text.Length;
+                        endIndex = text.Length - i;
                     }
 
                     string matchedString = text.Substring(i, endIndex);
                     Console.WriteLine(matchedString);
-                    i += jump;
+
+                    i += (endIndex - 1);
                 }
             }
 
