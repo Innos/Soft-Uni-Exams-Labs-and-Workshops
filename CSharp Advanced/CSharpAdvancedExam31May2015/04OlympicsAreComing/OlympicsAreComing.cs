0263819d3fdbf4b1e812ffdb1d10c04cd3343d27
Innos <victor.d.kazakov@gmail.com>
2015-05-31 17:21:18 +0300

    added 31 May 2015 Exam

---
+++ CSharp Advanced/CSharpAdvancedExam31May2015/04OlympicsAreComing/OlympicsAreComing.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+class OlympicsAreComing
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string,Dictionary<string,int>> statistics = new Dictionary<string,Dictionary<string,int>>();
+        string input;
+        while((input = Console.ReadLine()) != "report")
+        {
+            input = Regex.Replace(input, @"\s+", " ");
+            string[] tokens = input.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+            string name = tokens[0].Trim();
+            string country = tokens[1].Trim();
+            if(!statistics.ContainsKey(country))
+            {
+                statistics.Add(country, new Dictionary<string, int>());
+            }
+            if(!statistics[country].ContainsKey(name))
+            {
+                statistics[country].Add(name, 0);
+            }
+            statistics[country][name] = statistics[country][name] + 1;    
+        }
+        statistics = statistics.OrderByDescending(x => x.Value.Values.Sum()).ToDictionary(x=>x.Key,y => y.Value);
+        foreach (var country in statistics)
+        {
+            Console.WriteLine("{0} ({1} participants): {2} wins",country.Key,country.Value.Keys.Count,country.Value.Values.Sum());
+        }
+    }
+}
+
