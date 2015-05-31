63e724b7e708e549ed45c8b0796b010e600db44d
Innos <victor.d.kazakov@gmail.com>
2015-05-31 17:21:18 +0300

    added 31 May 2015 Exam

---
+++ CSharp Advanced/CSharp Advanced Exam 31 May 2015/CSharpAdvancedExam31May2015/01CommandInterpreter/CommandInterpreter.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class CommandInterpreter
+{
+    static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder();
+        string input = Console.ReadLine();
+        List<string> collection = collection = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+        while ((input = Console.ReadLine()) != "end")
+        {
+            int start;
+            int count;
+            string tempString;
+            List<string> temp = new List<string>();
+            string[] commands = input.Split();
+            switch (commands[0])
+            {
+                case "reverse":
+                    start = int.Parse(commands[2]);
+                    count = int.Parse(commands[4]);
+                    if (IsValidOrder(collection, start, count))
+                    {
+                        for (int i = start; i < start + count; i++)
+                        {
+                            temp.Add(collection[i]);
+                        }
+                        temp.Reverse();
+                        collection.RemoveRange(start, count);
+                        collection.InsertRange(start, temp);
+                    }
+                    else
+                    {
+                        sb.AppendLine("Invalid input parameters.");
+                    }
+                    break;
+                case "sort":
+                    start = int.Parse(commands[2]);
+                    count = int.Parse(commands[4]);
+                    if (IsValidOrder(collection, start, count))
+                    {
+                        for (int i = start; i < start + count; i++)
+                        {
+                            temp.Add(collection[i]);
+                        }
+                        temp.Sort();
+                        collection.RemoveRange(start, count);
+                        collection.InsertRange(start, temp);
+                    }
+                    else
+                    {
+                        sb.AppendLine("Invalid input parameters.");
+                    }
+                    break;
+                case "rollLeft":
+                    count = int.Parse(commands[1]);
+                    if (IsValidRoll(collection, count))
+                    {
+                        for (int i = 0; i < (count%collection.Count); i++)
+                        {
+                            tempString = collection[0];
+                            for (int l = 0; l < collection.Count - 1; l++)
+                            {
+                                collection[l] = collection[l + 1];
+                            }
+                            collection[collection.Count - 1] = tempString;
+                        }
+                    }
+                    else
+                    {
+                        sb.AppendLine("Invalid input parameters.");
+                    }
+                    break;
+                case "rollRight":
+                    count = int.Parse(commands[1]);
+                    if (IsValidRoll(collection, count))
+                    {
+                        for (int i = 0; i < (count % collection.Count); i++)
+                        {
+                            tempString = collection.Last();
+                            for (int l = collection.Count - 1; l > 0; l--)
+                            {
+                                collection[l] = collection[l - 1];
+                            }
+                            collection[0] = tempString;
+                        }
+                    }
+                    else
+                    {
+                        sb.AppendLine("Invalid input parameters.");
+                    }
+                    break;
+            }
+        }
+        Console.Write(sb.ToString());
+        Console.WriteLine("[{0}]", String.Join(", ", collection));
+    }
+    static bool IsValidOrder(List<string> collection, int start, int count)
+    {
+        if (start < 0 || start >= collection.Count ||
+            count < 0 ||
+            start + count > collection.Count)
+        {
+            return false;
+        }
+        return true;
+    }
+    static bool IsValidRoll(List<string> collection, int count)
+    {
+        if (count < 0)
+        {
+            return false;
+        }
+        return true;
+    }
+}
+
---
+++ CSharp Advanced/CSharp Advanced Exam 31 May 2015/CSharpAdvancedExam31May2015/03TextTransformer/TextTransformer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+class TextTransformer
+{
+    static void Main(string[] args)
+    {
+        string input;
+        StringBuilder sb = new StringBuilder();
+        while((input = Console.ReadLine()) != "burp")
+        {
+            sb.Append(input);
+        }
+        string text = sb.ToString();
+        string pattern = @"(?:\$(?<result>[^\$\%\&\']+?)\$|\%(?<result>[^\$\%\&\']+?)\%|\&(?<result>[^\$\%\&\']+?)\&|\'(?<result>[^\$\%\&\']+?)\')";
+        text = Regex.Replace(text, @"\n", "");
+        text = Regex.Replace(text,@"\s+"," ");
+        MatchCollection matches = Regex.Matches(text, pattern);
+        StringBuilder result = new StringBuilder();
+        foreach (Match match in matches)
+        {
+            char sign = match.Value.First();
+            int offset = 0;
+            char[] res = match.Groups["result"].Value.ToCharArray();
+            switch(sign)
+            {
+                case '$': offset = 1; break;
+                case '%': offset = 2; break;
+                case '&': offset = 3; break;
+                case '\'': offset = 4; break;
+            }
+            for (int i = 0; i < res.Length; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    res[i] = (char)(res[i] + offset);
+                }
+                else
+                {
+                    res[i] = (char)(res[i] - offset);
+                }
+                result.Append(res[i]);
+            }
+            result.Append(' ');
+        }
+        Console.WriteLine(result.ToString());
+    }
+}
+
